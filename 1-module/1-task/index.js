7fe6ebe36f3b49c63cdcbb0de83122fe0cca2474
Julia Andreeva <julandre@protonmail.com>
2020-01-23 03:04:44 +0300

    factorial function using sycle for

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,13 @@
  * @param {number} n
  * @returns {number}
  */
+// Julia Andreeva
 function factorial(n) {
-
-}
+  let f=1;
+  if (n>=1) {
+  for (let i=1; i<=n; i++) {
+    f=f*i};
+  }
+   else if (n==0) {f=1} 
+   else {f='nan'};   
+   return f}
