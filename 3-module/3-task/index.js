be3e5b367db728c160b884f01acce96a50076584
Julia Andreeva <julandre@protonmail.com>
2020-02-07 01:36:02 +0300

    Верблюжья нотация

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,8 @@
  * @returns {string}
  */
 function camelize(str) {
-}
+    return str
+      .split('-') 
+      .map( (str, index) => index == 0 ? str : str[0].toUpperCase() + str.slice(1))
+      .join(''); 
+  }
